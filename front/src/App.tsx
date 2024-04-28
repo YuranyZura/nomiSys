b54e7966c16af940fc580cd612bddf5f6c34f9c1
cos4h <olvadis2004@gmail.com>
2024-04-27 21:46:23 -0500

    add tailwind css

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div>
-      <p>hola</p>
+    <div className="bg-slate-900 h-screen w-screen flex justify-center">
+      <h1 className="text-3xl font-bold underline text-white content-center">
+        Hello world!
+      </h1>
     </div>
   );
 }
