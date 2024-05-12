acf5b0f354add4764ba454f3ff1745a029c5325b
[Yurany] <yuranysanchezmar18@gmail.com>
2024-05-11 20:32:17 -0500

    resolver conflicto

--- front/src/components/PropietaryRegister.tsx
+++ front/src/components/PropietaryRegister.tsx
@@ -1,5 +1,6 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { Card, CardContent, CardTitle, CardHeader } from "./ui/card";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import {
