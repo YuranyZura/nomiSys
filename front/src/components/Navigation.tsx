b140553b958dc33008f97be788214cdfebe9d1e5
[Yurany] <yuranysanchezmar18@gmail.com>
2024-06-01 12:31:02 -0500

    boton registrar empresa

--- front/src/components/Navigation.tsx
+++ front/src/components/Navigation.tsx
@@ -2,6 +2,7 @@ import { auth } from "@/config/firebase";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { GoSignOut } from "react-icons/go";
+import { BsBuildingsFill, BsReceiptCutoff } from "react-icons/bs";
 
 const Navigation = () => {
   const [user, setUser] = useState(auth.currentUser);
@@ -18,7 +19,7 @@ const Navigation = () => {
   };
 
   return (
-    <header className="bg-sky-600 w-1/12 relative h-screen py-2 pr-2">
+    <header className="bg-sky-600 w-1/12 relative h-screen py-2 pr-0 flex flex-col justify-between ">
       <div className="flex flex-col">
         <img
           src={user?.photoURL ?? ""}
@@ -34,18 +35,26 @@ const Navigation = () => {
           Ver cuenta
         </Link>
       </div>
-      <div className="flex justify-center items-center h-[25vh] my-px-">
+      <div className="flex justify-center items-center flex-col text-white">
         <Link
-          className="border-2 border-solid border-red-600"
+          className="border-y-2 border-solid border-blue-950 w-full text-center py-2 hover:bg-blue-500"
           to="/dashboard/empresas"
         >
+          <BsBuildingsFill className="mx-auto text-white" size={25} />
           Empresas
+        </Link>
+        <Link
+          className="border-y-2 border-solid border-blue-950 w-full text-center py-2 hover:bg-blue-500"
+          to="/dashboard/nomina"
+        >
+          <BsReceiptCutoff className="mx-auto text-white" size={25} />
+          Nomina
         </Link>
       </div>
       <nav></nav>
       <div className="absolute bottom-0 mb-2 w-full flex justify-center">
         <button
-          className="text-white mx-auto font-semibold"
+          className="text-white mx-auto font-italic"
           onClick={handleSignOut}
         >
           <GoSignOut size={28} className="mx-auto" />
