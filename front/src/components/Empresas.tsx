732bd2e228ef1297efe570e4469780db1d098b94
cos4h <olvadis2004@gmail.com>
2024-06-14 17:25:12 -0500

    listando empresas

--- front/src/components/Empresas.tsx
+++ front/src/components/Empresas.tsx
@@ -12,39 +12,49 @@ import { MdEmail } from "react-icons/md";
 import { FaBarcode } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 import { FaPhoneAlt } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { getEmpresasUser } from "@/service/empresa";
+import { useStore } from "@/store/user";
+import { auth } from "@/config/firebase";
+import { syncBuiltinESMExports } from "module";
 
 const Empresas = () => {
   const navigate = useNavigate();
+  const [empresasData, setEmpresasData] = useState<any>([]);
+  const empresas = useStore((state: any) => state.empresas);
+  const setEmpresas = useStore((state: any) => state.setEmpresas);
+  const [userAuth, setUserAuth] = useState<any>(null);
 
   function handleSubmit() {
     navigate("/dashboard/RegistroEmpresa");
   }
-  const empresa = [
-    {
-      name: "Zoomac",
-      description: "Construcción",
-      email: "construccion@gmail.com",
-      RUT: "1258",
-      address: "Apartado CL 98 #12-23",
-      phone: "321987456",
-    },
-    {
-      name: "Inversiones Ahsarch S.A.S. Zomac",
-      description: "Construcción",
-      email: "ahsley3110@gmail.com",
-      RUT: "901299575-5",
-      address: "Chigorodó Urb Montecarlos Manzana B4 #26",
-      phone: "3156987423",
-    },
-    {
-      name: "Solo Maiz",
-      description: "Venta de envueltos de choclo",
-      email: "solomaiz@gmail.com",
-      RUT: "02365",
-      address: "Apartado Calle #3 Bloque",
-      phone: "3225649807",
-    },
-  ];
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user: any) => {
+      setUserAuth(user);
+      if (user) {
+        // Ahora fetchEmpresas se llama después de que setUserAuth se ha llamado
+        const empresasDB = await getEmpresasUser(user.email);
+        setEmpresas(empresasDB);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    const fetchEmpresas = async () => {
+      const data = await empresas;
+      setEmpresasData(data);
+    };
+
+    fetchEmpresas();
+  }, [empresas]);
+
+  const fetchEmpresas = async () => {
+    const empresasDB = await getEmpresasUser(userAuth?.email ?? "");
+    setEmpresas(empresasDB);
+  };
 
   return (
     <div className="p-6 w-full ">
@@ -62,39 +72,46 @@ const Empresas = () => {
       </h1>
       <div className="space- p-4  w-full flex justify-center">
         <div className="grid grid-cols-3 sm:grid-cols-2 md:grid-cols-4 gap-4 max-w-7xl mx-auto">
-          {empresa.map((empresa, index) => (
-            <Card key={index} className="w-full flex flex-col justify-between">
-              <CardHeader>
-                <CardTitle>{empresa.name}</CardTitle>
-                <CardDescription>{empresa.description}</CardDescription>
-              </CardHeader>
-              <CardContent className="flex flex-col gap-2">
-                <div className="flex space-x-2">
-                  <MdEmail size={25} className="text-sky-500" />
-                  <p>{empresa.email}</p>
-                </div>
-                <div className="flex space-x-2">
-                  <FaBarcode size={25} className="text-sky-500" />
-                  <p>{empresa.RUT}</p>
-                </div>
-                <div className="flex space-x-2">
-                  <FaLocationDot size={25} className="text-sky-500" />
-                  <p>{empresa.address}</p>
-                </div>
-                <div className="flex space-x-2">
-                  <FaPhoneAlt size={20} className="text-sky-500" />
-                  <p>{empresa.phone}</p>
-                </div>
-              </CardContent>
-              <CardFooter className="mx-auto">
-                <div>
-                  <Button className="mt-2  bg-slate-800 hover:bg-blue-500 text-lg">
-                    Ver más
-                  </Button>
-                </div>
-              </CardFooter>
-            </Card>
-          ))}
+          {empresasData.length > 0 &&
+            empresasData.map((empresa, index) => (
+              <Card
+                key={index}
+                className="w-full flex flex-col justify-between"
+              >
+                <CardHeader>
+                  <CardTitle>{empresa.nombreEmpresa}</CardTitle>
+                  <CardDescription>
+                    {empresa.descripcionEmpresa}
+                  </CardDescription>
+                  {console.log(empresa)}
+                </CardHeader>
+                <CardContent className="flex flex-col gap-2">
+                  <div className="flex space-x-2">
+                    <MdEmail size={25} className="text-sky-500" />
+                    <p>{empresa.correo}</p>
+                  </div>
+                  <div className="flex space-x-2">
+                    <FaBarcode size={25} className="text-sky-500" />
+                    <p>{empresa.rut}</p>
+                  </div>
+                  <div className="flex space-x-2">
+                    <FaLocationDot size={25} className="text-sky-500" />
+                    <p>{empresa.direccion}</p>
+                  </div>
+                  <div className="flex space-x-2">
+                    <FaPhoneAlt size={20} className="text-sky-500" />
+                    <p>{empresa.telefono}</p>
+                  </div>
+                </CardContent>
+                <CardFooter className="mx-auto">
+                  <div>
+                    <Button className="mt-2  bg-slate-800 hover:bg-blue-500 text-lg">
+                      Ver más
+                    </Button>
+                  </div>
+                </CardFooter>
+              </Card>
+            ))}
         </div>
       </div>
     </div>
--- front/src/service/empresa.ts
+++ front/src/service/empresa.ts
@@ -1,9 +1,27 @@
 import { db } from "@/config/firebase";
-import { setDoc, doc as docFirebase } from "firebase/firestore";
-import { v4 as uuid } from "uuid"; 
+import {
+  setDoc,
+  doc as docFirebase,
+  getDocs,
+  query,
+  collection,
+  where,
+} from "firebase/firestore";
+import { v4 as uuid } from "uuid";
+import { getUserByEmail } from "./user";
 
 export async function saveEmpresa(values) {
   const id = uuid();
-  const docRef = docFirebase(db, "empresa", id); 
+  const docRef = docFirebase(db, "empresa", id);
   const registroempresa = await setDoc(docRef, values);
+}
+
+export async function getEmpresasUser(userEmail: string) {
+  const userDB = await getUserByEmail(userEmail);
+  const snapshot = await getDocs(
+    query(collection(db, "empresa"), where("userDoc", "==", userDB.doc))
+  );
+  const empresas = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+
+  return empresas;
 }
